22aed972130edd2464db618813c3e1bfa8179db9
DrPiccoloMD <60704345+DrPiccoloMD@users.noreply.github.com>
2020-02-05 13:54:14 -0600

    Add files via upload

---
+++ LegalOfficeSignIn/Form1.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace LegalOfficeSignIn
+{
+    partial class frmApptScheduler
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblDuty = new System.Windows.Forms.Label();
+            this.lblAppt = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblCard = new System.Windows.Forms.Label();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.nameBox = new System.Windows.Forms.TextBox();
+            this.dutyBox = new System.Windows.Forms.ComboBox();
+            this.apptBox = new System.Windows.Forms.ComboBox();
+            this.chkLegal = new System.Windows.Forms.CheckBox();
+            this.cardBox = new System.Windows.Forms.ComboBox();
+            this.noteBox = new System.Windows.Forms.TextBox();
+            this.dtpDate = new System.Windows.Forms.DateTimePicker();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.lblNotes = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.lblTime = new System.Windows.Forms.Label();
+            this.dtpTime = new System.Windows.Forms.DateTimePicker();
+            this.attnBox = new System.Windows.Forms.ComboBox();
+            this.lblAttn = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(12, 5);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(359, 37);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Schedule Appointment";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(77, 48);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(38, 13);
+            this.lblName.TabIndex = 1;
+            this.lblName.Text = "Name:";
+            // 
+            // lblDuty
+            // 
+            this.lblDuty.AutoSize = true;
+            this.lblDuty.Location = new System.Drawing.Point(50, 74);
+            this.lblDuty.Name = "lblDuty";
+            this.lblDuty.Size = new System.Drawing.Size(65, 13);
+            this.lblDuty.TabIndex = 2;
+            this.lblDuty.Text = "Duty Status:";
+            // 
+            // lblAppt
+            // 
+            this.lblAppt.AutoSize = true;
+            this.lblAppt.Location = new System.Drawing.Point(46, 101);
+            this.lblAppt.Name = "lblAppt";
+            this.lblAppt.Size = new System.Drawing.Size(69, 13);
+            this.lblAppt.TabIndex = 3;
+            this.lblAppt.Text = "Appointment:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(25, 126);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(90, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Legal Assistance:";
+            // 
+            // lblCard
+            // 
+            this.lblCard.AutoSize = true;
+            this.lblCard.Location = new System.Drawing.Point(56, 151);
+            this.lblCard.Name = "lblCard";
+            this.lblCard.Size = new System.Drawing.Size(59, 13);
+            this.lblCard.TabIndex = 5;
+            this.lblCard.Text = "Card Type:";
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.Location = new System.Drawing.Point(82, 175);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(33, 13);
+            this.lblDate.TabIndex = 6;
+            this.lblDate.Text = "Date:";
+            // 
+            // nameBox
+            // 
+            this.nameBox.Location = new System.Drawing.Point(118, 45);
+            this.nameBox.Name = "nameBox";
+            this.nameBox.Size = new System.Drawing.Size(156, 20);
+            this.nameBox.TabIndex = 0;
+            // 
+            // dutyBox
+            // 
+            this.dutyBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.dutyBox.FormattingEnabled = true;
+            this.dutyBox.Location = new System.Drawing.Point(118, 71);
+            this.dutyBox.Name = "dutyBox";
+            this.dutyBox.Size = new System.Drawing.Size(121, 21);
+            this.dutyBox.TabIndex = 1;
+            // 
+            // apptBox
+            // 
+            this.apptBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.apptBox.FormattingEnabled = true;
+            this.apptBox.Location = new System.Drawing.Point(118, 98);
+            this.apptBox.Name = "apptBox";
+            this.apptBox.Size = new System.Drawing.Size(199, 21);
+            this.apptBox.TabIndex = 2;
+            // 
+            // chkLegal
+            // 
+            this.chkLegal.AutoSize = true;
+            this.chkLegal.Location = new System.Drawing.Point(117, 125);
+            this.chkLegal.Name = "chkLegal";
+            this.chkLegal.Size = new System.Drawing.Size(15, 14);
+            this.chkLegal.TabIndex = 3;
+            this.chkLegal.UseVisualStyleBackColor = true;
+            this.chkLegal.CheckedChanged += new System.EventHandler(this.chkLegal_CheckedChanged);
+            // 
+            // cardBox
+            // 
+            this.cardBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cardBox.Enabled = false;
+            this.cardBox.FormattingEnabled = true;
+            this.cardBox.Location = new System.Drawing.Point(118, 148);
+            this.cardBox.Name = "cardBox";
+            this.cardBox.Size = new System.Drawing.Size(156, 21);
+            this.cardBox.TabIndex = 4;
+            // 
+            // noteBox
+            // 
+            this.noteBox.Location = new System.Drawing.Point(117, 257);
+            this.noteBox.Multiline = true;
+            this.noteBox.Name = "noteBox";
+            this.noteBox.Size = new System.Drawing.Size(200, 74);
+            this.noteBox.TabIndex = 8;
+            // 
+            // dtpDate
+            // 
+            this.dtpDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpDate.Location = new System.Drawing.Point(118, 175);
+            this.dtpDate.Name = "dtpDate";
+            this.dtpDate.Size = new System.Drawing.Size(121, 20);
+            this.dtpDate.TabIndex = 5;
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.Location = new System.Drawing.Point(150, 337);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(75, 23);
+            this.btnEnter.TabIndex = 9;
+            this.btnEnter.Text = "Enter";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            this.btnEnter.Click += new System.EventHandler(this.btnEnter_Click);
+            // 
+            // lblNotes
+            // 
+            this.lblNotes.AutoSize = true;
+            this.lblNotes.Location = new System.Drawing.Point(35, 260);
+            this.lblNotes.Name = "lblNotes";
+            this.lblNotes.Size = new System.Drawing.Size(80, 13);
+            this.lblNotes.TabIndex = 15;
+            this.lblNotes.Text = "Attorney Notes:";
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Location = new System.Drawing.Point(82, 201);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(33, 13);
+            this.lblTime.TabIndex = 16;
+            this.lblTime.Text = "Time:";
+            // 
+            // dtpTime
+            // 
+            this.dtpTime.CustomFormat = "hh:mm tt";
+            this.dtpTime.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpTime.Location = new System.Drawing.Point(118, 201);
+            this.dtpTime.Name = "dtpTime";
+            this.dtpTime.ShowUpDown = true;
+            this.dtpTime.Size = new System.Drawing.Size(121, 20);
+            this.dtpTime.TabIndex = 6;
+            // 
+            // attnBox
+            // 
+            this.attnBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.attnBox.Enabled = false;
+            this.attnBox.FormattingEnabled = true;
+            this.attnBox.Location = new System.Drawing.Point(117, 227);
+            this.attnBox.Name = "attnBox";
+            this.attnBox.Size = new System.Drawing.Size(156, 21);
+            this.attnBox.TabIndex = 7;
+            // 
+            // lblAttn
+            // 
+            this.lblAttn.AutoSize = true;
+            this.lblAttn.Location = new System.Drawing.Point(66, 230);
+            this.lblAttn.Name = "lblAttn";
+            this.lblAttn.Size = new System.Drawing.Size(49, 13);
+            this.lblAttn.TabIndex = 18;
+            this.lblAttn.Text = "Attorney:";
+            // 
+            // frmApptScheduler
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(375, 367);
+            this.Controls.Add(this.attnBox);
+            this.Controls.Add(this.lblAttn);
+            this.Controls.Add(this.dtpTime);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.lblNotes);
+            this.Controls.Add(this.btnEnter);
+            this.Controls.Add(this.dtpDate);
+            this.Controls.Add(this.noteBox);
+            this.Controls.Add(this.cardBox);
+            this.Controls.Add(this.chkLegal);
+            this.Controls.Add(this.apptBox);
+            this.Controls.Add(this.dutyBox);
+            this.Controls.Add(this.nameBox);
+            this.Controls.Add(this.lblDate);
+            this.Controls.Add(this.lblCard);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblAppt);
+            this.Controls.Add(this.lblDuty);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblTitle);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmApptScheduler";
+            this.ShowIcon = false;
+            this.Text = "Appointment Scheduler";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblDuty;
+        private System.Windows.Forms.Label lblAppt;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblCard;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.TextBox nameBox;
+        private System.Windows.Forms.ComboBox dutyBox;
+        private System.Windows.Forms.ComboBox apptBox;
+        private System.Windows.Forms.CheckBox chkLegal;
+        private System.Windows.Forms.ComboBox cardBox;
+        private System.Windows.Forms.TextBox noteBox;
+        private System.Windows.Forms.DateTimePicker dtpDate;
+        private System.Windows.Forms.Button btnEnter;
+        private System.Windows.Forms.Label lblNotes;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.DateTimePicker dtpTime;
+        private System.Windows.Forms.ComboBox attnBox;
+        private System.Windows.Forms.Label lblAttn;
+    }
+}
+
---
+++ LegalOfficeSignIn/Form1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Data.OleDb;
+using System.Windows.Forms;
+
+namespace LegalOfficeSignIn
+{
+
+    public partial class frmApptScheduler : Form
+    {
+        static string conString = "Provider=Microsoft.Jet.OLEDB.4.0; Data Source = " + Directory.GetCurrentDirectory() + "/LegalOfficeAppts.mdb; Jet OLEDB:Database Password=legaloffice1;";
+        OleDbConnection con = new OleDbConnection(conString);
+        OleDbCommand cmd;
+
+        string legalAssist = "NO";
+        string[] dutyStatus = new String[6] { "Active", "Retired", "Deployed", "Dependents", "Reserve", "Other" };
+        string[] apptType = new String[5] { "Will", "Power of Attorney", "Medical Directive", "Legal Assistance", "Will/Power of Attorney/Medical Directive" };
+        string[] cardType = new String[5] { "AD CAC", "DD Form 2 (Reserve/Guard)", "DD Form 2 (Retired)", "DD Form 1173", "DoD Civilian Retiree Card" };
+
+        public frmApptScheduler()
+        {
+            InitializeComponent();
+
+            for (int i = 0; i < 6; i++)
+            {
+                dutyBox.Items.Add(dutyStatus[i]);
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                apptBox.Items.Add(apptType[i]);
+                cardBox.Items.Add(cardType[i]);
+            }
+
+            try
+            {
+                con.Open();
+                string fillAttn = "SELECT [AttnName] FROM [Attorneys] WHERE Available = True AND [AttnName] IS NOT NULL";
+                cmd = new OleDbCommand(fillAttn, con);
+
+                OleDbDataReader reader = cmd.ExecuteReader();
+                while(reader.Read())
+                {
+                    attnBox.Items.Add(reader["AttnName"].ToString());
+                    attnBox.Enabled = true;
+                }
+                
+            }
+            catch
+            {
+                MessageBox.Show("Unexpected database error has occured.", "Unknown Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        private void chkLegal_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkLegal.Checked == true)
+            {
+                cardBox.Enabled = true;
+                legalAssist = "YES";
+            }
+            else
+            {
+                cardBox.Enabled = false;
+                cardBox.SelectedIndex = -1;
+                legalAssist = "NO";
+            }
+        }
+
+        private void btnEnter_Click(object sender, EventArgs e)
+        {
+            inputValidation();
+        }
+
+        public void inputValidation()
+         {
+            TimeSpan startTues = new TimeSpan(14, 0, 0);
+            TimeSpan endTues = new TimeSpan(15, 0, 0);
+            TimeSpan startThurs = new TimeSpan(9, 0, 0);
+            TimeSpan endThurs = new TimeSpan(10, 0, 0);
+
+            if (nameBox.Text == "")
+            {
+                MessageBox.Show("Please enter a name.", "Invalid Entry", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            else if (dutyBox.SelectedIndex == -1)
+            {
+                MessageBox.Show("Please select a duty status.", "Invalid Entry", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            else if (apptBox.SelectedIndex == -1)
+            {
+                MessageBox.Show("Please select an appointment type.", "Invalid Entry", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            else if (chkLegal.Checked == true && cardBox.SelectedIndex == -1)
+            {
+                MessageBox.Show("Please select a card type.", "Invalid Entry", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            else if (attnBox.SelectedIndex == -1)
+            {
+                MessageBox.Show("Please select an attorney.", "Invalid Entry", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            else if (dtpDate.Value.DayOfWeek == DayOfWeek.Saturday || dtpDate.Value.DayOfWeek == DayOfWeek.Sunday)
+            {
+                MessageBox.Show("Legal Office is closed on weekends. Please select a week day.", "Invalid Entry", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            else if (dtpDate.Value.DayOfWeek == DayOfWeek.Tuesday && (dtpTime.Value.TimeOfDay < startTues || dtpTime.Value.TimeOfDay >= endTues))
+            {
+                MessageBox.Show("Retiree appointments are only available from 1400 to 1500 on Tuesdays. Please select another day.", "Invalid Entry", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            else if (dtpDate.Value.DayOfWeek == DayOfWeek.Thursday && (dtpTime.Value.TimeOfDay < startThurs || dtpTime.Value.TimeOfDay >= endThurs))
+            {
+                MessageBox.Show("Retiree appointments are only available from 0900 to 1000 on Thursdays. Please select another day.", "Invalid Entry", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            else
+            {
+                addAppt();
+            }
+        }
+
+        private bool validAppt()
+        {
+            con.Open();
+
+            string checkBlackoutDate = "SELECT BlackoutDate FROM tblBlackout WHERE (BlackoutDate = @BlackoutDate) AND (BlackoutTimeStart) IS NULL AND (BlackoutTimeEnd) IS NULL";
+            string checkBlackoutTime = "SELECT BlackoutDate FROM tblBlackout WHERE (BlackoutDate = @BlackoutDate) AND (@BlackoutTime >= (BlackoutTimeStart)) AND (@BlackoutTime <= (BlackoutTimeEnd))";
+            string checkSame = "SELECT COUNT(*) FROM ApptSchedule WHERE (Attorney = @Attorney) AND (ApptDate = @ApptDate) AND (ApptTime = @ApptTime)";
+
+            OleDbCommand cmdDate = new OleDbCommand(checkBlackoutDate, con);
+            OleDbCommand cmdTime = new OleDbCommand(checkBlackoutTime, con);
+            OleDbCommand cmdSame = new OleDbCommand(checkSame, con);
+
+            cmdTime.Parameters.AddWithValue("@BlackoutDate", dtpDate.Text);
+            cmdTime.Parameters.AddWithValue("@BlackoutTime", dtpTime.Text);
+            cmdDate.Parameters.AddWithValue("@BlackoutDate", dtpDate.Text);
+            cmdSame.Parameters.AddWithValue("@Attorney", attnBox.Text);
+            cmdSame.Parameters.AddWithValue("@ApptDate", dtpDate.Text);
+            cmdSame.Parameters.AddWithValue("@ApptTime", dtpTime.Text);
+
+            OleDbDataReader drTime = cmdTime.ExecuteReader();
+            OleDbDataReader drDate = cmdDate.ExecuteReader();
+            OleDbDataReader drSame = cmdSame.ExecuteReader();
+
+            if (drTime.HasRows)
+            {
+                MessageBox.Show("Legal Office will not be open at that time.  Please choose another time.", "Closed", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                con.Close();
+                return false;
+            }
+            else if (drDate.HasRows)
+            {
+                MessageBox.Show("Legal Office will not be open that day.  Please choose another day.", "Closed", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                con.Close();
+                return false;
+            }
+            else if (drSame.Read())
+            {
+                int count = (int)drSame[0];
+                if (count > 0)
+                {
+                    MessageBox.Show("Attorney will be unavailable at that time. Please select another time.", "Attorney Is Unavailable", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    con.Close();
+                    return false;
+                }
+                con.Close();
+                return true;
+            }
+            else
+            {
+                con.Close();
+                return true;
+            }
+        }
+
+        public void addAppt()
+        {
+                if (validAppt() == true)
+            {
+                try
+                {
+                    {
+                        con.Open();
+                        string sql = "INSERT INTO ApptSchedule (CustomerName, DutyStatus, Appointment, LegalAssist, CardType, ApptDate, ApptTime, Attorney, AttorneyNotes) VALUES (@CUSTOMERNAME, @DUTYSTATUS, @APPOINTMENT, @LEGALASSIST, @CARDTYPE, @APPTDATE, @APPTTIME, @ATTORNEY, @ATTORNEYNOTES)";
+                        cmd = new OleDbCommand(sql, con);
+
+                        cmd.Parameters.AddWithValue("@CUSTOMERNAME", nameBox.Text);
+                        cmd.Parameters.AddWithValue("@DUTYSTATUS", dutyBox.Text);
+                        cmd.Parameters.AddWithValue("@APPOINTMENT", apptBox.Text);
+                        cmd.Parameters.AddWithValue("@LEGALASSIST", legalAssist);
+                        cmd.Parameters.AddWithValue("@CARDTYPE", cardBox.Text);
+                        cmd.Parameters.AddWithValue("@APPTDATE", dtpDate.Text);
+                        cmd.Parameters.AddWithValue("@APPTTIME", dtpTime.Text);
+                        cmd.Parameters.AddWithValue("@ATTORNEY", attnBox.Text);
+                        cmd.Parameters.AddWithValue("@ATTORNEYNOTES", noteBox.Text);
+
+                        cmd.ExecuteNonQuery();
+
+                        MessageBox.Show("Your appointment has been scheduled.", "Thank You", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                        nameBox.Text = "";
+                        dutyBox.SelectedIndex = -1;
+                        apptBox.SelectedIndex = -1;
+                        chkLegal.Checked = false;
+                        cardBox.SelectedIndex = -1;
+                        dtpDate.Text = "";
+                        dtpTime.Text = "";
+                        attnBox.SelectedIndex = -1;
+                        noteBox.Text = "";
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("Unexpected error has occured.", "Unknown Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                finally
+                {
+                    con.Close();
+                }
+            }
+        }
+    }
+}
